aec5b2c7e1e2fd57df84589f8d89555e31ff185a
VenusKing1026 <jinxing0033221@163.com>
2025-03-11 00:32:45 +0100

    complete variables

---
+++ Practical4/variables.py
@@ -0,0 +1,21 @@
+a=15 #walk to bus stop
+b=75 #bus journey
+c=a+b  #total
+d=90 #drive
+e=5 #walk from car park
+f=d+e #car based
+
+print(c>f)
+
+
+#f is longer 
+#bus is quicker
+
+X=True
+Y=False
+W=X and Y
+#   
+# X 1 1 0 0
+# Y 1 0 1 0
+# w 1 0 0 0
+
