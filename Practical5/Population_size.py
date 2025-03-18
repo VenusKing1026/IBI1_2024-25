a815789df8b823820d5f05a4764c2e8e3db480d6
JX <jinxing0033221@163.com>
2025-03-18 11:48:51 +0800

    ' finish Practicle on 3.18

---
+++ Practical5/Population_size.py
@@ -0,0 +1,35 @@
+# initialize data and labels
+# prepare a figure contain 2 plots
+# set each of them
+# show
+
+
+
+
+import matplotlib.pyplot as plt 
+
+# intialize data
+uk_countries=[57.11,3.13,1.91,5.45]
+zj_neighbours=[65.77,41.88,45.28,61.27,85.15]
+
+# initialize label
+uk_countries_lables=['England','Wales','Northern Ireland','Scotland']
+zj_neighbours_lables=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
+
+# output 2 lists
+print(uk_countries,zj_neighbours)
+
+# create a figure contains 2 plot
+fig,(plt1,plt2)=plt.subplots(1,2,figsize=(12,6))
+
+# set plot 1
+plt1.pie(uk_countries,labels=uk_countries_lables,explode=[0.1,0,0,0],startangle=30,autopct='%1.1f%%',wedgeprops={'edgecolor':'blue','linewidth':1,'antialiased':True})
+plt1.set_title('UK_countries')
+
+# set plot 2
+plt2.pie(zj_neighbours,labels=zj_neighbours_lables,explode=[0.1,0,0,0,0.1],startangle=30,autopct='%1.1f%%',wedgeprops={'edgecolor':'blue','linewidth':1,'antialiased':True})
+plt2.set_title('zhejiang_neighbours')
+
+#show
+plt.tight_layout()
+plt.show()
---
+++ Practical5/Programming_language_popularity.py
@@ -0,0 +1,24 @@
+# impot matplot
+# initialize dictionary
+# get what users want to know
+# give the value corresponding to the key
+
+# set plot size,title,label
+# draw bar plot 
+
+import matplotlib.pyplot as plt # import
+
+popularity_data={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5} #initialize
+PLanguage=str(input('Language:')) # get the key
+print(popularity_data[Language]) # give users data they want
+
+pl=plt.bar(popularity_data.keys(),popularity_data.values()) # initialize barplot
+
+# set labels title for plot
+plt.ylabel('Percentage')
+plt.xlabel('Language')
+plt.title('programming language popularity')
+plt.show()
+
+
+
