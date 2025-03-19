0b464662a740daa8952eeecc3d9691c9a348eb83
JX <jinxing0033221@163.com>
2025-03-19 14:55:00 +0800

    add defalt value for dict

--- Practical5/Programming_language_popularity.py
+++ Practical5/Programming_language_popularity.py
@@ -8,9 +8,10 @@
 
 import matplotlib.pyplot as plt # import
 
-popularity_data={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5} #initialize
+popularity_data={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5,} #initialize
 Language=str(input('Language:')) # get the key
-print(popularity_data[Language]) # give users data they want
+print(popularity_data.get(Language,"No such language")) # give users data they want
+print(popularity_data)
 
 pl=plt.bar(popularity_data.keys(),popularity_data.values()) # initialize barplot
 
