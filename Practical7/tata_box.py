c6b4b7a27e0ac66de38c13165929d3450da6baae
JX <jinxing0033221@163.com>
2025-04-02 15:44:09 +0800

    add comment

--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -14,17 +14,17 @@ seq=''
 gene=[0]
 import re
              
-for line in file:           #      
-    if re.search(r'^>',line):
+for line in file:           #   get every gene name and sequence in the dict   
+    if re.search(r'^>',line): # if it is the first line in one gene,store the sequence for last gene and store name for this gene
         dict[gene[0]]=seq
         gene=re.findall(r'gene:(\S+)',line)
         seq=''
-    if not(re.search(r'^>',line)):
+    if not(re.search(r'^>',line)): # if it is not the first line in one gene, store the sequence as one line
         seq+=str(line[:-1])
 del dict[0]
 
 tata_gene={}
-for gene1 in dict.keys():
+for gene1 in dict.keys():    # if it is tata gene, store it into tata_gene
     if re.search(r'TATA[AT]A[AT]',dict[gene1]):
         tata_gene[gene1]=dict[gene1]
     else:
@@ -32,7 +32,7 @@ for gene1 in dict.keys():
 
 output=open('tata_genes.fa','w')
 
-for gene2,seq2 in tata_gene.items():
+for gene2,seq2 in tata_gene.items():  # output
     output.write('>'+str(gene2)+'\n')
     output.write(str(seq2)+'\n')
 
